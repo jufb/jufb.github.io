167f6727c2a4250bf4a1e3cba6403671380698e2
JuFB <fborges.j@gmail.com>
2022-04-20 15:31:39 -0400

    Published v3.0.0

--- src/views/about.js
+++ src/views/about.js
@@ -7,27 +7,27 @@ export function About() {
     <main id="About" role="main" className='bg-light'>
         <Container className='text-center jumbotron'>
         <h1>About</h1>
-        Software Engineer. 
-        <Button variant="link" href="https://1drv.ms/b/s!AquVyDo2_0wmrBEq6lEqC-qJ5gzS" target='_blank' style={{paddingTop: 5, paddingLeft: 4}}>
-            Open my resume/CV <BoxArrowUpRight color="currentColor" size="12" />
-        </Button>
+          Software Engineer. 
+          <Button variant="link" href="https://1drv.ms/b/s!AquVyDo2_0wmgqchpz1XjfWcoUWXwA" target='_blank' style={{paddingTop: 5, paddingLeft: 4}}>
+              Open my resume/CV <BoxArrowUpRight color="currentColor" size="12" />
+          </Button>
         </Container>
         <Container>
         <Row>
             <Col>
-            <h4>Hi, my name is Juliana...</h4>
-            And I live in Ontario, Canada.<br/>
-            I have accumulated 10+ years of experience as a Software Engineer.<br/>
-            In my career, I was involved in the Project Life Cycle Phases. Consequently, Business Analysis and Project Management were also part of my experience.
-            <p>Opportunities that promote diversity, equality, and inclusion are my goals.</p>
+            <h3>Hi, my name is Juliana...</h3>
+              And I live in Ontario, Canada.<br/>
+              I have accumulated 10+ years of experience as a Software Engineer.<br/>
+              In my career, I was involved in the Project Life Cycle Phases. Consequently, Business Analysis and Project Management were also part of my experience.
+              <p>Opportunities that promote diversity, equality, and inclusion are my goals.</p>
+            </Col>
+            <Col className='text-center'>
+              <Image className="figure-img" roundedCircle src="https://static.wixstatic.com/media/249783_9be0ce1d760d45af9206292fff26a7c1~mv2.jpg" alt='Juliana F. Borges picture.' />
             </Col>
             <Col>
-              <Image fluid="true" className="figure-img" roundedCircle src="https://static.wixstatic.com/media/249783_9be0ce1d760d45af9206292fff26a7c1~mv2.jpg" alt='Juliana F. Borges picture.' />
-            </Col>
-            <Col className="text-right">
               {dataSkills.map((array, i) => (
                 <div key={i}>
-                  <h5>{array.title}</h5>
+                  <h3>{array.title}</h3>
                   {array.skills.map((subarray, x) => (
                     <Badge key={x} bg="secondary">
                       {subarray}
@@ -38,10 +38,10 @@ export function About() {
             </Col>
         </Row>
         <hr /> 
-        <blockquote className="blockquote">
-            <h3 className="font-italic">"Leaders must inspire and motivate to raise new leaders."</h3>
-            <h3 className="blockquote-footer">Juliana</h3>
-        </blockquote>
+        
+        <blockquote><h3>“Leaders must inspire and motivate to raise new leaders.”</h3></blockquote>
+        <cite>― Juliana</cite>
+        
         </Container>
     </main>
   );

32941364a3a7e1b4db44d007102bfdd6deca9e50
JuFB <fborges.j@gmail.com>
2022-06-25 15:03:40 -0400

    Published v3.1.1

--- src/views/about.js
+++ src/views/about.js
@@ -38,6 +38,9 @@ export function About() {
                 </div>
               ))}
 
+              <Button variant="link" href="https://www.credly.com/badges/d133ed2d-d8e5-4eed-b741-3ff7a8838f80/public_url" target='_blank'>
+                <Image className='certifications' src="https://static.wixstatic.com/media/249783_1a05b0ebae2042858b54a09d69ee67d0~mv2.png" alt='Microsoft certified Azure Fundamentals.' />
+              </Button>
               <Button variant="link" href="https://www.credly.com/badges/cd80d417-a02c-417b-bff5-b2c1dff94126/public_url" target='_blank'>
                 <Image className='certifications' src="https://static.wixstatic.com/media/249783_cb2c77e2cefd40e096f0d2268ab0c459~mv2.png" alt='Microsoft certified Azure AI Fundamentals.' />
               </Button>

9a65c759269ec86209bdca926aa808d1f1babcba
Juliana F. Borges <fborges.j@gmail.com>
2024-07-12 21:55:52 -0400

    Changed the about and contact sections

--- src/views/about.js
+++ src/views/about.js
@@ -8,16 +8,17 @@ export function About() {
         <div className='hero'>
           <Container className='text-center jumbotron'>
             <h1>About</h1>
-              <h5>Software Engineer.<br />
-              <Button variant="link" href="https://1drv.ms/b/s!AquVyDo2_0wmgqchpz1XjfWcoUWXwA" target='_blank' style={{paddingTop: 5, paddingLeft: 4}}>
-                  Open my resume <BoxArrowUpRight color="currentColor" size="20" />
+              <h5>Software Engineer and Indie Game Developer.<br />
+              <Button variant="link" href="https://www.linkedin.com/in/fborgesj" target='_blank' style={{paddingTop: 5, paddingLeft: 4}}>
+                  Open my LinkedIn <BoxArrowUpRight color="currentColor" size="20" />
               </Button>
               </h5>
           </Container>
           <Container>
-            <p>Hi, I'm Juliana. I am a professional Software Engineer with over ten years of experience in the IT industry, with solid knowledge in full-stack applications. Plus, I also hold knowledge in business analysis and project management.</p>
-            <p>Technology has been part of my life since my childhood. Visual Basic was my first coding experience during my secondary school in Data Processing. I published my first website in my teens.</p>
-            <p>Currently, I live in Ontario, Canada. I enjoy spending my free time having long walks with my family, playing videogames, and learning and exploring what's new in tech.</p>
+            <p>Hi, I'm Juliana, a professional Software Engineer with over ten years of experience in the IT industry, specializing in full-stack applications. Additionally, I have a solid background in business analysis and project management.</p>
+            <p>Technology has been part of my life since my childhood. Visual Basic was my first coding experience as part of my secondary school curriculum in Data Processing. Those early lines of code sparked a passion that has stayed with me ever since. During my teenage years, I proudly launched my very first website.</p>
+            <p>Fast-forward to today: I’m donning my indie game developer hat, crafting a brand new title.</p>
+            <p>On my free time, I enjoy playing sports with my family, playing videogames, and exploring what's new in tech.</p>
             <hr /> 
             <blockquote><h4>“Leaders must inspire and motivate to raise new leaders.”</h4></blockquote>
             <cite>― Juliana</cite>
--- src/views/contact.js
+++ src/views/contact.js
@@ -12,19 +12,16 @@ export function Contact() {
         <Button variant="link" href="mailto:fborges.j@gmail.com" target='_blank'>
           <Envelope color="currentColor" size="40" />
           <h4>Email</h4>
-            fborges.j@gmail.com
         </Button>
 
         <Button variant="link" href="https://www.linkedin.com/in/fborgesj" target='_blank'>
           <Linkedin color="currentColor" size="40" />
           <h4>LinkedIn</h4>
-            https://www.linkedin.com/in/fborgesj
         </Button>
 
         <Button variant="link" href="https://github.com/jufb" target='_blank'>
           <Github color="currentColor" size="40" />
           <h4>GitHub</h4>
-            https://github.com/jufb
         </Button>
       </Container>
     </main>
